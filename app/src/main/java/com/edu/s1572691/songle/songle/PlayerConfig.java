ec0434eaa257eee6858d5a2db4264b58dab45d25
Rusab <rusab36@gmail.com>
2017-12-01 19:50:19 +0000

    Now shows the correct percentage of words collected in the SongList activity. Also shows the correct Song No.s in the Maps Activity. (STILL NEED OPTIMIZATION)

---
+++ app/src/main/java/com/edu/s1572691/songle/songle/PlayerConfig.java
@@ -0,0 +1,14 @@
+package com.edu.s1572691.songle.songle;
+/**
+ * Created by Rusab Asher on 21/08/2017.
+ */
+
+public class PlayerConfig {
+
+    PlayerConfig() {
+
+    }
+    public static final String API_KEY = "AIzaSyDTD3MAG60QB5juQuM4FJgbFBP68ngHRLw";
+
+
+}
